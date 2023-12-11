ba4cad3439f5d2c52999d87d1de64f1da188594c
soo1e <gameswuon@gmail.com>
2023-12-12 00:15:54 +0900

    section 13

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,53 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, Headers } from "@nestjs/common";
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Post('token/access')
+  createTokenAccess(@Headers('authorization') rawToken: string) {
+    const token = this.authService.extractTokenFromHeader(rawToken, true);
+
+    const newToken = this.authService.rotatetoken(token, false);
+
+    // {accessToken: {token}}
+
+    return {
+      accessToken: newToken,
+    };
+  }
+
+  @Post('token/refresh')
+  createTokenRefresh(@Headers('authorization') rawToken: string) {
+    const token = this.authService.extractTokenFromHeader(rawToken, true);
+
+    const newToken = this.authService.rotatetoken(token, true);
+
+    // {accessToken: {token}}
+
+    return {
+      accessToken: newToken,
+    }
+  }
+
+
   @Post('login/email')
-  loginEmail(
-    @Body('email') email: string,
-    @Body('password') password: string,
+  postLoginEmail(
+    @Headers('authorization') rawToken: string,
   ) {
-    return this.authService.loginWithEmail({ email, password });
+    // email:password -> base64
+    // decode -> split
+
+    const token = this.authService.extractTokenFromHeader(rawToken, false);
+
+    const credentials = this.authService.decodeBasicToken(token);
+
+    return this.authService.loginWithEmail(credentials);
   }
 
   @Post('register/email')
-  registerEmail(
+  postRegisterEmail(
     @Body('nickname') nickname: string,
     @Body('email') email: string,
     @Body('password') password: string,
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,6 +7,39 @@ import * as bcrypt from "bcrypt";
 
 
 /**
+ * 토큰을 사용하게 되는 방식
+ *
+ * 1) 사용자가 로그인 또는 회원가입을 진행하면
+ *    accesssToken과 refreshToken을 발급받는다.
+ *
+ * 2) 로그인 할때는 Basic 토큰과 함께 요청을 보낸다.
+ *    Basic 토큰은 '이메일:패스워드'를 Base64로 인코딩한 형태이다.
+ *
+ * 3) 아무나 접근할 수 없는 정보 (private route)를 접근 할 때는
+ *    accessToken을 Header에 추가해서 요청과 함께 보낸다.
+ *    예) {authorization : 'Bearer {token}'}
+ *
+ * 4) 토큰과 요청을 함께 받은 서버는 토큰 검증을 통해 현재 요청을 보낸
+ *    사용자가 누구인지 알 수 있다.
+ *    예를 들어서 현재 로그인한 사용자가 작성한 포스트만 가져오려면
+ *    토큰의 sub 값에 입력되어있는 사용자의 포스트만 따로 필터링 할 수 있다.
+ *    특정 사용자의 토큰이 없다면 다른 사용자의 데이터에 접근을 하지 못한다.
+ *
+ * 5) 모든 토큰은 만료 기간이 있어 만료 기간이 지나면 새로 토큰을 발급받아야한다.
+ *    그렇지 않으면 jwtService.verity()에서 인증을 통과하지 못한다.
+ *    그러니 access 토큰을 새로 발급 받을 수 있는 /auth/token/access와
+ *    refresh 토큰을 새로 발급 받을 수 있는 /auth/token/refresh가 필요하다.
+ *
+ * 6) 토큰이 만료되면 각각의 토큰을 새로 발급 받을 수 있는 엔드포인트에 요청을 해서
+ *    새로운 토큰을 발급받고 새로운 토큰을 사용해서 private route에 접근한다.
+ *
+ * Header로 부터 토큰을 받을 때
+ *
+ * {authorization : 'Basic {token}'}
+ * {authorization : 'Bearer {token}'}
+ * /
+
+/**
  * 만드려는 기능
  *
  * 1) registerWithEmail
@@ -45,6 +78,67 @@ export class AuthService {
    * 2) sub -> id
    * 3) type -> access | refresh
    */
+  extractTokenFromHeader(header: string, isBearer: boolean) {
+    // 'Basic {token}' ->
+    // [Basic, {token}]
+    const splitToken = header.split(' ');
+
+    const prefix = isBearer ? 'Bearer' : 'Basic';
+
+    if(splitToken.length !== 2 || splitToken[0] !== prefix) {
+      throw new UnauthorizedException('잘못된 토큰입니다.');
+    }
+
+    const token = splitToken[1];
+
+    return token;
+  }
+
+  decodeBasicToken(base64String: string) {
+    const decoded = Buffer.from(base64String, 'base64').toString('utf8');
+
+    const split = decoded.split(':');
+
+    if(split.length !== 2) {
+      throw new UnauthorizedException('잘못된 유형의 토큰입니다.')
+    }
+
+    const email = split[0];
+    const password = split[1];
+
+    return {
+      email,
+      password,
+    }
+  }
+
+  // 토큰 검증
+  verifyToken(token: string){
+    return this.jwtService.verify(token, {
+      secret: JWT_SECRET,
+    })
+  }
+
+  //
+  rotatetoken(token: string, isRefreshToken: boolean) {
+    const decoded = this.jwtService.verify(token, {
+      secret: JWT_SECRET,
+    });
+
+    /**
+     * sub: id
+     * email: email
+     * type: 'access' | 'refresh'
+     */
+
+    if(decoded.type !== 'refresh') {
+      throw new UnauthorizedException('토큰 재발급은 Refresh Token만 가능합니다.');
+    }
+
+    return this.signToken({
+      ...decoded,
+    }, isRefreshToken);
+  }
 
   signToken(user: Pick<UsersModel, 'email' | 'id'>, isRefreshToken: boolean) {
     const payload = {
